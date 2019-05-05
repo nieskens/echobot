5e07227b59824e1081cf7e58a7321bc740ae1535
Berry Nieskens <berry@nieskens.com>
2019-05-05 15:20:22 +0200

    Added random response time before bot's answer

--- echobot.py
+++ echobot.py
@@ -1,6 +1,9 @@
 # COMPUTING & COGNITION
 # ECHOBOT v0.1
 
+import time
+import random
+
 # Define a function that responds to a user's message: respond
 def respond(message):
     # Concatenate the user's message to the end of a standard bot respone
@@ -10,7 +13,9 @@ def respond(message):
 # Define a function that sends the message and prints both user and bot's answers
 def send_message(message):
     print("YOU: {}".format(message))
+    time.sleep(random.randint(1,3))
     print("ECHOBOT: {}".format(respond(message)))
-
 # Call the function send_message to start the diaglogue
-send_message("Hi, how are you?")
+
+print("Hi, how are you?")
+send_message(input())
