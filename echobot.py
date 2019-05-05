152bef116395b07669c5ff6622e95ca17d95ab94
Berry Nieskens <berry@nieskens.com>
2019-05-05 14:53:07 +0200

    Added send_message and dialogue template

--- echobot.py
+++ echobot.py
@@ -5,8 +5,12 @@
 def respond(message):
     # Concatenate the user's message to the end of a standard bot respone
     bot_message = "I can hear you! You said: {}".format(message)
-    # Return the result
     return bot_message
 
-# Call the function respond an pass a message
-print(respond("Hi!"))
+# Define a function that sends the message and prints both user and bot's answers
+def send_message(message):
+    print("YOU: {}".format(message))
+    print("ECHOBOT: {}".format(respond(message)))
+
+# Call the function send_message to start the diaglogue
+send_message("Hi, how are you?")
